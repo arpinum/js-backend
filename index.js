77f7074accf598c1eca30a6052e9e5b83bd25419
Michael Borde <michael.borde@ubimail.fr>
2015-12-29 18:02:06 +0100

    module init refactoring

--- index.js
+++ index.js
@@ -3,30 +3,34 @@
 module.exports = {
   MongoDatabase: require('./lib/database/MongoDatabase'),
 
-  CommandBus: require('./lib/ddd/buses/CommandQueryBus'),
-  QueryBus: require('./lib/ddd/buses/CommandQueryBus'),
-  EventBus: require('./lib/ddd/buses/EventBus'),
-  ConflictingEntityError: require('./lib/ddd/errors/ConflictingEntityError'),
-  EntityNotFoundError: require('./lib/ddd/errors/EntityNotFoundError'),
+  CommandBus: require('./lib/ddd/bus/CommandQueryBus'),
+  QueryBus: require('./lib/ddd/bus/CommandQueryBus'),
+  EventBus: require('./lib/ddd/bus/EventBus'),
+  ConflictingEntityError: require('./lib/ddd/error/ConflictingEntityError'),
+  EntityNotFoundError: require('./lib/ddd/error/EntityNotFoundError'),
   BaseRepository: require('./lib/ddd/repository/BaseRepository'),
+  RepositoryInitializer: require('./lib/ddd/repository/RepositoryInitializer'),
+
   BaseCommand: require('./lib/ddd/command/BaseCommand'),
+  CommandInitializer: require('./lib/ddd/command/CommandInitializer'),
 
   FakeApplication: require('./lib/test/FakeApplication'),
   FakeResponse: require('./lib/test/FakeResponse'),
   MemoryDatabase: require('./lib/test/MemoryDatabase'),
   MemoryRepository: require('./lib/test/MemoryRepository'),
 
-  FunctionalError: require('./lib/tools/errors/FunctionalError'),
-  TechnicalError: require('./lib/tools/errors/TechnicalError'),
+  FunctionalError: require('./lib/tools/error/FunctionalError'),
+  TechnicalError: require('./lib/tools/error/TechnicalError'),
   LoggerFactory: require('./lib/tools/LoggerFactory'),
 
-  ClientError: require('./lib/web/errors/ClientError'),
-  ConflictingResourceError: require('./lib/web/errors/ConflictingResourceError'),
-  ResourceNotFoundError: require('./lib/web/errors/ResourceNotFoundError'),
-  ServerError: require('./lib/web/errors/ServerError'),
-  UnauthorizedError: require('./lib/web/errors/UnauthorizedError'),
-  WebError: require('./lib/web/errors/WebError'),
+  ClientError: require('./lib/web/error/ClientError'),
+  ConflictingResourceError: require('./lib/web/error/ConflictingResourceError'),
+  ResourceNotFoundError: require('./lib/web/error/ResourceNotFoundError'),
+  ServerError: require('./lib/web/error/ServerError'),
+  UnauthorizedError: require('./lib/web/error/UnauthorizedError'),
+  WebError: require('./lib/web/error/WebError'),
 
-  UnhandledErrorMiddleware: require('./lib/web/middlewares/UnhandledErrorMiddleware'),
+  UnhandledErrorMiddleware: require('./lib/web/middleware/UnhandledErrorMiddleware'),
+  ResourceInitializer: require('./lib/web/resource/ResourceInitializer'),
   BodyValidator: require('./lib/web/validation/BodyValidator')
 };
